f80637da5fafdb501c3941ee9df66835cf825164
Pol Milian <polcius@gmail.com>
2019-12-22 14:30:31 +0100

    basic main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { Normalize } from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
+import ProductGrid from "./ProductGrid";
+import Navbar from "./Navbar";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-size: 16px;
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 2rem;
+  }
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
-    </div>
+    <>
+      <Normalize />
+      <GlobalStyle />
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <ProductGrid />
+      </main>
+    </>
   );
 };
 
