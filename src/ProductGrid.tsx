8cd5275c3318be81a9d3a166fb71a95a7db0bfac
Pol Milian <polcius@gmail.com>
2019-12-28 18:52:19 +0100

    working with real API, data fetching logic

--- src/ProductGrid.tsx
+++ src/ProductGrid.tsx
@@ -1,7 +1,7 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 import styled from "styled-components";
 import Card from "./Card";
-import mockData from "./products.json";
+import axios from "axios";
 
 const GridWrapper = styled.section`
   display: grid;
@@ -13,21 +13,60 @@ type ProductGridProps = {
   sizeFilter: string;
 };
 
+type DataResponseItem = {
+  index: number;
+  isSale: boolean;
+  isExclusive: false;
+  price: string;
+  productImage: string;
+  productName: string;
+  size: string[];
+};
+
 const ProductGrid: FunctionComponent<ProductGridProps> = ({ sizeFilter }) => {
-  const filteredItems = mockData.filter(item => item.size.includes(sizeFilter));
+  // For a real app, all the data fetching logic could be extracted into a custom hook, and even a separate file.
+  const [data, setData] = useState<DataResponseItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios(`${process.env.REACT_APP_API_URL}`);
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const filteredItems =
+    data.length > 0 && data.filter(item => item.size.includes(sizeFilter));
+
   return (
-    <GridWrapper>
-      {filteredItems.map(product => (
-        <Card
-          key={product.index}
-          image={product.productImage}
-          name={product.productName}
-          price={product.price}
-          isSale={product.isSale}
-          isExclusive={product.isExclusive}
-        />
-      ))}
-    </GridWrapper>
+    <>
+      {isError && <div>Something went wrong...</div>}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <GridWrapper>
+          {filteredItems &&
+            filteredItems.map(product => (
+              <Card
+                key={product.index}
+                image={product.productImage}
+                name={product.productName}
+                price={product.price}
+                isSale={product.isSale}
+                isExclusive={product.isExclusive}
+              />
+            ))}
+        </GridWrapper>
+      )}
+    </>
   );
 };
 
